969973834fb7a32ad583e43b0c79507f5a7249db
lucassouto <lucasinfologos@gmail.com>
2019-06-19 00:04:23 -0300

    fix(health check): Change HealthCheck permission_classes to AllowAny

--- loan_management/api/views.py
+++ loan_management/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import status, viewsets
+from rest_framework import status, permissions, viewsets
 from rest_framework.response import Response
 
 
 class HealthCheck(viewsets.ViewSet):
+    permission_classes = (permissions.AllowAny,)
+
     def list(self, request):
         return Response({'message': 'healthy'}, status=status.HTTP_200_OK)
