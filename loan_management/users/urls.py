cc4ff6b36f247604a6ad15da0ebf62f4607ef1f7
lucassouto <lucasinfologos@gmail.com>
2019-06-19 02:06:10 -0300

    fix(import): Import User from models

--- loan_management/users/urls.py
+++ loan_management/users/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import UsersViewSet
+from .views import UserViewSet
 
 app_name = 'users'
 
 router = SimpleRouter()
-router.register('users', UsersViewSet, basename='users')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = router.urls
--- loan_management/users/views.py
+++ loan_management/users/views.py
@@ -4,6 +4,6 @@ from .models import User
 from .serializers import UserSerializer
 
 
-class UsersViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
