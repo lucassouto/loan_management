4726b1e7c472013a013d9202ac140176b10b7e31
lucassouto <lucasinfologos@gmail.com>
2019-06-29 20:54:43 -0300

    feat(contract): Create propertys amount_due and amount_paid; Change interest_rate field

--- loan_management/loans/models.py
+++ loan_management/loans/models.py
@@ -1,4 +1,5 @@
 import uuid
+from decimal import Decimal
 
 from django.db import models
 from model_utils.models import TimeStampedModel
@@ -10,11 +11,26 @@ from ..users.models import User
 class Contract(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    interest_rate = models.IntegerField()
+    interest_rate = models.DecimalField(max_digits=3, decimal_places=2)
     ip_address = models.GenericIPAddressField()
     submission_date = models.DateTimeField()
     bank = models.ForeignKey(Bank, on_delete=models.PROTECT, related_name='contracts')
     client = models.ForeignKey(User, on_delete=models.PROTECT, related_name='contracts')
+
+    @property
+    def amount_due(self) -> Decimal:
+        value_with_interest = self.amount + (self.amount * self.interest_rate)
+
+        if not self.payments.exists():
+            return value_with_interest
+
+        return round(value_with_interest - self.amount_paid, 2)
+
+    @property
+    def amount_paid(self) -> Decimal:
+        payments = self.payments.values('payment_amount').aggregate(models.Sum('payment_amount'))
+
+        return payments['payment_amount__sum']
 
 
 class Payment(TimeStampedModel):
