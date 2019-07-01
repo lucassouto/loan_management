be4ec552b68e4737388995a907bf51aad0201b59
lucassouto <lucasinfologos@gmail.com>
2019-07-01 01:23:48 -0300

    feat(docs): Add SECURITY_DEFINITIONS to docs settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,4 +175,15 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
 }
 
-LOGIN_URL = '/admin/'
+SWAGGER_SETTINGS = {
+    'DOC_EXPANSION': 'none',
+    'LOGIN_URL': '/admin/login/',
+    'LOGOUT_URL': '/admin/logout/',
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+      }
+   }
+}
